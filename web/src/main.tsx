dde39de419e84463320f241f8bac2dc868573dcd
NazBilgic <cloudofnaz@gmail.com>
2025-09-19 22:34:55 +0100

    Update main.tsx with fixes

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App'
 import './index.css'
 import { setApiBase } from './lib/apiClient'
 
-// 👇 API base URL'i .env'den alıyoruz
+
 setApiBase(import.meta.env.VITE_API_BASE);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
