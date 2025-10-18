6d6c7b769ae6689b22852472aaafb37defba5e12
Jevonney Irwin <jevz.devops@gmail.com>
2025-10-18 21:02:34 +0100

    renamed webapp title and added build command to skip vite tests for easy build

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    // skip type-checking (Vite will still build and bundle fine)
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
+  },
 })
